278921ff92117ccc8ca27d1036be671f99cf109a
poojithreddy18 <poojithreddy66@gmail.com>
2021-08-08 16:31:34 +1000

    practical4

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,66 +1,61 @@
 const cardList = [
-    {
-        title: "Australia",
-        image: "images/au.jpg",
-        link: "Top Australian Universities",
-        desciption: "Deakin University <br> Monash University <br> university of Melbourne <br> Australian National University"
-    },
-    {
-        title: "United Kingdom",
-        image: "images/uk.png",
-        link: "Best UK Universities",
-        desciption: "Oxford University <br> University of Cambridge <br> Manchester University <br> London University"
-    },
-    {
-        title: "Canada",
-        image: "images/canada.png",
-        link: "Top Canadian Universities",
-        desciption: "McGill University <br> University of Alberta <br> University Of Waterloo <br> York University"
-    },
-    {
-        title: "Ireland",
-        image: "images/ireland.jpg",
-        link: "Best Universities in Ireland",
-        desciption: "Maynooth University <br> Dublin City University <br> National University of Ireland <br> Trinity College Dublin"
-    }
+
 ]
 const clickMe = () => {
-    alert("Thanks for clicking me. Hope you have a nice day!")
+    alert("Thanks for clicking, Have a Nice Day..!")
+}
+
+const addProjectToApp = (project) => {
+    $.ajax({
+        url: 'api/projects',
+        data: project,
+        type: 'POST',
+        success: (result) => {
+            alert(result.message);
+            location.reload();
+        }
+    })
 }
 
 const submitForm = () => {
     let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.email = $('#email').val();
-    formData.Phone_Number = $('#Phone_Number').val();
-    formData.username = $('#username').val();
-    formData.password = $('#password').val();
+    formData.title = $('#title').val();
+    formData.image = $('#image').val();
+    formData.link = $('#link').val();
+    formData.description = $('#description').val();
 
     console.log("Form Data Submitted: ", formData);
+    addProjectToApp(formData);
+}
+
+const getProjects = () => {
+    $.get('/api/project',(response) => {
+        if(response.statusCode==200){
+            console.log(response)
+            addCards(response.data); 
+        }
+    })
 }
 
 const addCards = (items) => {
     items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">' +
-            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">' +
-            '</div><div class="card-content">' +
-            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
-            '<div class="card-reveal">' +
-            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
-            '<p class="card-text">' + item.desciption + '</p>' +
-            '</div></div></div>';
-        $("#card-section").append(itemToAppend)
+        let itemToAppend = '<div class="col s4 center-align">'+
+    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+    '</div><div class="card-content">'+
+    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+    '<div class="card-reveal">'+
+        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+        '<p class="card-text">'+item.desciption+'</p>'+
+      '</div></div></div>';
+      $("#card-section").append(itemToAppend)
     });
 }
 
-
-
-$(document).ready(function () {
+$(document).ready(function(){
     $('.materialboxed').materialbox();
-    $('#formSubmit').click(() => {
+    $('#formSubmit').click(()=>{
         submitForm();
     })
-    addCards(cardList);
+    getProjects();
     $('.modal').modal();
-});
+  });
--- server.js
+++ server.js
@@ -1,12 +1,68 @@
+require('dotenv').config()
 var express = require("express")
 var app = express()
+const MongoClient = require('mongodb').MongoClient;
+let projectCollection;
+
+const uri = "mongodb+srv://sit725-2021-t2-prac4:Poojith@sit725-2021-t2-prac4.lnldy.mongodb.net/sit725-2021-t2-prac4?retryWrites=true&w=majority"
+const client = new MongoClient(uri,{ useNewUrlParser: true})
 
 app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const createColllection = (collectionName) => {
+    client.connect((err,db) => {
+        projectCollection = client.db().collection(collectionName);
+        if(!err) {
+            console.log('MongoDB Connected')
+        }
+        else {
+            console.log("DB Error: ", err);
+            process.exit(1);
+        }
+    })
+} 
+
+const cardList = [
+
+]
+
+const insertProjects = (project,callback) => {
+    projectCollection.insert(project,callback);
+}
+
+const getProjects = (callback) => {
+    projectCollection.find({}).toArray(callback);
+}
+
+app.get('/api/projects',(req,res) => {
+    getProjects((err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message:err})
+        }
+        else {
+             res.json({statusCode: 200, message:"Success", data: result})
+        }
+     })
+})
+
+app.post('/api/projects',(req,res) => {
+    console.log("New Project add", req.body)
+    var newProject = req.body;
+    insertProjects(newProject,(err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+      }
+        else {
+            res.json({statusCode: 200, message:"Project has Successfully placed", data: result})
+        }
+     })
+})
+
 var port = process.env.port || 5000;
 
 app.listen(port, () => {
-    console.log("App listening to: " + port)
+    console.log("App listening to: " + port);
+    createColllection("New country")
 })
